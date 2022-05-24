8cafbeeaad091ae56d36db762c20c82dde56a486
xmNuc <xmnuc@o2.pl>
2022-05-24 20:17:58 +0200

    Add maping title and thumbnail

--- src/components/Posts/OnePost.tsx
+++ src/components/Posts/OnePost.tsx
@@ -13,7 +13,6 @@ import './OnePost.css';
 import favYes from '../../img/favyes.png';
 import favNo from '../../img/favno.png';
 import { vidId } from './YtParser';
-import { isTemplateTail } from 'typescript';
 
 export const OnePost = () => {
   const [favorites, SetFavorites] = useState(false);
@@ -29,8 +28,8 @@ export const OnePost = () => {
         `https://www.googleapis.com/youtube/v3/videos?part=id%2C+snippet&id=${vidId}&key=${process.env.REACT_APP_API_KEY}`
       );
       const ytData = await res.json();
-      setData(ytData);
-      console.log(res);
+      setData(ytData.items);
+      // console.log(ytData.items);
     })();
   }, []);
 
@@ -38,31 +37,41 @@ export const OnePost = () => {
     <h2>Loading data...</h2>;
   }
 
+  data.map((data: any) => console.log(data.snippet.title));
+
   const handleOpenModal = () => {};
   return (
     <>
-      <Card className="my-card" onClick={handleOpenModal}>
-        <img
-          className="favorites"
-          src={favorites ? favYes : favNo}
-          alt="favorites"
-          onClick={handleFavorites}
-        />
-        <CardImg alt="Card image cap" src="https://picsum.photos/318/180" top width="100%" />
-        <CardBody>
-          <CardTitle tag="h5">Card title{data.kind}</CardTitle>
-          <CardSubtitle className="mb-2 text-muted" tag="h6">
-            Card subtitle
-          </CardSubtitle>
-          <CardText>
-            This is a wider card with supporting text below as a natural lead-in to additional
-            content. This content is a little bit longer.
-          </CardText>
-          <Row>
-            <Button>Delete</Button>
-          </Row>
-        </CardBody>
-      </Card>
+      {data.map((data: any) => (
+        <Card key={data.id} className="my-card" onClick={handleOpenModal}>
+          <img
+            className="favorites"
+            src={favorites ? favYes : favNo}
+            alt="favorites"
+            onClick={handleFavorites}
+          />
+
+          <CardImg
+            alt="Card image cap"
+            src={data.snippet.thumbnails.standard.url}
+            top
+            width="100%"
+          />
+          <CardBody>
+            <CardTitle tag="h5">{data.snippet.title}</CardTitle>
+            <CardSubtitle className="mb-2 text-muted" tag="h6">
+              Card subtitle
+            </CardSubtitle>
+            <CardText>
+              This is a wider card with supporting text below as a natural lead-in to additional
+              content. This content is a little bit longer.
+            </CardText>
+            <Row>
+              <Button>Delete</Button>
+            </Row>
+          </CardBody>
+        </Card>
+      ))}
     </>
   );
 };
