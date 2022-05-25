f98472990e7199a2b7a3f1f02ccb6549d077cefc
xmNuc <xmnuc@o2.pl>
2022-05-25 03:37:37 +0200

    Update one post

--- src/components/Posts/OnePost.tsx
+++ src/components/Posts/OnePost.tsx
@@ -17,6 +17,7 @@ import { videoId } from './YtParser';
 export const OnePost = () => {
   const [favorites, SetFavorites] = useState(false);
   const [data, setData] = useState<[] | any>([]);
+  const [vid, setVid] = useState(videoId);
 
   const handleFavorites = () => {
     SetFavorites(!favorites);
@@ -24,25 +25,33 @@ export const OnePost = () => {
   useEffect(() => {
     (async () => {
       const res = await fetch(
-        `https://www.googleapis.com/youtube/v3/videos?part=id%2C+snippet&id=${videoId}&key=${process.env.REACT_APP_API_KEY}`
+        `https://www.googleapis.com/youtube/v3/videos?part=id%2C+snippet&id=${vid}&key=${process.env.REACT_APP_API_KEY}`
       );
       const ytData = await res.json();
       setData(ytData.items);
-      console.log(ytData.items);
+      // console.log(ytData.items);
     })();
-  }, []);
+  }, [vid]);
+  // console.log(vid);
 
   if (!data) {
     <h2>Loading data...</h2>;
   }
 
-  // data.map((data: any) => console.log(data.snippet.title));
+  // data.map((data: any) => console.log(data));
+  // data.map((data: any) => console.log(data.id));
 
   const handleOpenModal = () => {};
+
+  const removeItem = (asd: string) => {
+    console.log(`Remove item  ${asd}`);
+    setVid(vid.filter((one: any) => one.asd !== asd));
+  };
+
   return (
     <>
       {data.map((data: any) => (
-        <Card key={data.id} className="my-card" onClick={handleOpenModal}>
+        <Card key={data.id} id={data.id} className="my-card" onClick={handleOpenModal}>
           <img
             className="favorites"
             src={favorites ? favYes : favNo}
@@ -60,8 +69,11 @@ export const OnePost = () => {
               This is a wider card with supporting text below as a natural lead-in to additional
               content. This content is a little bit longer.
             </CardText>
+            <p>Today is: {new Date().toLocaleDateString()}</p>
             <Row className="d-flex align-items-end mt-auto">
-              <Button className="d-flex justify-content-center">Delete</Button>
+              <Button className="d-flex justify-content-center" onClick={() => removeItem(data.id)}>
+                Delete
+              </Button>
             </Row>
           </CardBody>
         </Card>
--- src/components/Posts/YtParser.tsx
+++ src/components/Posts/YtParser.tsx
@@ -4,13 +4,13 @@ const url = demoDb;
 
 // data.map((data: any) => console.log(data.snippet.title));
 
-export const videoId = url
+export let videoId = url
   .map((item: string) =>
     item.match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/)
   )
   .map((item: string) => item[item.length - 1]);
 
-console.log(videoId);
+// console.log(videoId);
 
 // console.log(videoid);
 // const ids = videoid.map((i: string) =>
