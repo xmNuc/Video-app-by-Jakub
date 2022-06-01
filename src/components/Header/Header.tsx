e76b9359a3e73b02fcc467796e43622cdccba5a4
xmNuc <xmnuc@o2.pl>
2022-06-01 08:52:42 +0200

    Fixed header view on smal screan

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,17 +6,17 @@ import './Header.css';
 export const Header = () => {
   return (
     <>
-      <header className="fixed-top d-flex align-items-center justify-content-between row flex-wrap">
-        <h2 className="d-flex justify-content-center col-2">
+      <header className="row fixed-top d-flex align-items-center justify-content-between  flex-wrap">
+        <h2 className="logo d-flex justify-content-center col-xl-2 col-sm-6">
           <a href="/">
             <strong>Video</strong> App{' '}
           </a>{' '}
           <p>by Jakub</p>
         </h2>
-        <div className="col-6 ">
+        <div className="btns d-flex justify-content-center align-items-center flex-wrap col-6 col-sm-6">
           <Buttons />
         </div>
-        <div className="pt-3 col">
+        <div className="input pt-3 col-xl-4 col-sm-12">
           <UrlForm />
         </div>
       </header>
