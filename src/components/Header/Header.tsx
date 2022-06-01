4b603bffb220fcea49988c1707704c2147281a58
xmNuc <xmnuc@o2.pl>
2022-06-01 08:58:19 +0200

    Update css

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,13 +7,13 @@ export const Header = () => {
   return (
     <>
       <header className="row fixed-top d-flex align-items-center justify-content-between  flex-wrap">
-        <h2 className="logo d-flex justify-content-center col-xl-2 col-sm-6">
+        <h2 className="logo d-flex justify-content-center col-xl-2 col-sm-12">
           <a href="/">
             <strong>Video</strong> App{' '}
           </a>{' '}
           <p>by Jakub</p>
         </h2>
-        <div className="btns d-flex justify-content-center align-items-center flex-wrap col-6 col-sm-6">
+        <div className="btns d-flex justify-content-center align-items-center flex-wrap col-xl-6 col-sm-12">
           <Buttons />
         </div>
         <div className="input pt-3 col-xl-4 col-sm-12">
